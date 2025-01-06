93706a49e550b0a974699b205380b62a093d0b71
Guido Barosio <guido@bravo47.com>
2025-01-06 01:21:13 -0300

    more

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,11 @@
- 
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: 'node',
+  transform: {},
+  extensionsToTreatAsEsm: ['.js'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
+
+export default config; 
